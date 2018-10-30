3dc601147fc820a679b579dac99f6989388c74e9
Zack C <zackcozine@yahoo.com>
2018-10-29 21:59:47 -0500

    pretty barebones but everything works how it should

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Peanut Butter Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Doube Cheeseburger", 1);
